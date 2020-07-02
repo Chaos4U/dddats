038088b58ab74f52de45c8c760cb1a3a402c9145
ChaoD <59477195+Chaos4U@users.noreply.github.com>
2020-07-02 18:57:18 +0500

    Update kde.sh

--- script/installer/kde.sh
+++ script/installer/kde.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # xorg
-sudo xbps-install -Su
-sudo xbps-install -S xorg
+sudo xbps-install -Syu
+sudo xbps-install -Sy xorg
 
 # kde
-sudo xbps-install -S kde5 kde5-baseapps
+sudo xbps-install -Sy kde5 kde5-baseapps
 
 # dm
 sudo xbps-install -Sy lightdm lightdm-gtk3-greeter
