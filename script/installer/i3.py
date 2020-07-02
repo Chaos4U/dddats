9940b60cfb39cf52399bf6967625177c401ad156
ChaoD <59477195+Chaos4U@users.noreply.github.com>
2020-07-02 18:54:33 +0500

    Update i3.py

--- script/installer/i3.py
+++ script/installer/i3.py
@@ -1,20 +1,25 @@
 import os
 
 # base
-sudo xbps-install -Syu
-sudo xbps-install -Sy xorg
+os.system('sudo xbps-install -Syu')
+os.system('sudo xbps-install -Sy xorg')
 
 # i3 install
-sudo xbps-install -Sy i3status dmenu nano sakura
+os.system('sudo xbps-install -Sy i3status dmenu nano sakura')
+
 # i3 background
-sudo xbps-install -Sy feh
+os.system('sudo xbps-install -Sy feh')
+
 
 # some apps
-sudo xbps-install -Sy lxappearance
-sudo xbps-install -Sy papirus-icon-theme
-sudo xbps-install -Sy arc-theme
-sudo xbps-install -Sy mousepad
+os.system('sudo xbps-install -Sy lxappearance')
+os.system('sudo xbps-install -Sy papirus-icon-theme')
+os.system('sudo xbps-install -Sy arc-theme')
+os.system('sudo xbps-install -Sy mousepad')
 
-sudo xbps-install -Sy lightdm lightdm-gtk3-greeter
-sudo ln -s /etc/sv/dbus /var/service/dbus
-sudo ln -s /etc/sv/lightdm /var/service/lightdm
+os.system('sudo xbps-install -Sy lightdm lightdm-gtk3-greeter')
+os.system('sudo ln -s /etc/sv/dbus /var/service/dbus')
+os.system('sudo ln -s /etc/sv/lightdm /var/service/lightdm')
+
+
+
