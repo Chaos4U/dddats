70a0b53de2f5530ad6a6976ad07722d7a67ac1de
ChaoD <59477195+Chaos4U@users.noreply.github.com>
2020-07-02 18:32:58 +0500

    Update install.py

--- script/install.py
+++ script/install.py
@@ -1,5 +1,6 @@
 import os
 from installer.some import login
+from time import sleep
 
 if(login == 0): # first login installing desktop
   from installer.some import cpu
@@ -8,12 +9,15 @@ if(login == 0): # first login installing desktop
   wm = input("Which wm you want(i3,xfce,kde)?: ")
   if(wm == "i3"): # i3 DE
     os.popen('sh ~/dddats/script/installer/i3.sh')
+    sleep(20)
     login = 1
   if(wm == "xfce"): #xfce WM
     os.popen('sh ~/dddats/script/installer/xfce.sh')
+    sleep(20)
     login = 1
   if(wm == "kde"): #kde WM
     os.popen('sh ~/dddats/script/installer/kde.sh')
+    sleep(20)
     login = 1
 while(login == 1):
     todo = input("What you wanna do? (drivers,sysupd,pkginstall): ")
