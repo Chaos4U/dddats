1c9ebf7604dd65b258cd8ed42413a4bb4874a048
ChaoD <59477195+Chaos4U@users.noreply.github.com>
2020-07-02 15:21:28 +0500

    Add files via upload

--- script/installer/i3.sh
+++ script/installer/i3.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 # base
-sudo xbps-install -S xorg
+sudo xbps-install -Sy xorg
 
 # i3 install
-sudo xbps-install -Su
-sudo xbps-install -S i3status dmenu nano sakura
+sudo xbps-install -Syu
+sudo xbps-install -Sy i3status dmenu nano sakura
 # i3 background
-sudo xbps-install -S feh
+sudo xbps-install -Sy feh
 
 # some apps
-sudo xbps-install -S lxappearance
-sudo xbps-install -S papirus-icon-theme
-sudo xbps-install -S arc-theme
-sudo xbps-install -S mousepad
+sudo xbps-install -Sy lxappearance
+sudo xbps-install -Sy papirus-icon-theme
+sudo xbps-install -Sy arc-theme
+sudo xbps-install -Sy mousepad
+
+sudo xbps-install -Sy lightdm lightdm-gtk3-greeter
+sudo ln -s /etc/sv/dbus /var/service/dbus
+sudo ln -s /etc/sv/lightdm /var/service/lightdm
--- script/installer/kde.sh
+++ script/installer/kde.sh
@@ -6,3 +6,8 @@ sudo xbps-install -S xorg
 
 # kde
 sudo xbps-install -S kde5 kde5-baseapps
+
+# dm
+sudo xbps-install -Sy lightdm lightdm-gtk3-greeter
+sudo ln -s /etc/sv/dbus /var/service/dbus
+sudo ln -s /etc/sv/lightdm /var/service/lightdm
